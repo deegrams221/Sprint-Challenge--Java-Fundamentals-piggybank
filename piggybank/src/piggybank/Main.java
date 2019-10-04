4e28104255114627149dbf8c44217da05372d8c0
Diana Grams <deegrams221@gmail.com>
2019-10-04 09:12:37 -0600

    Initial project setup

---
+++ piggybank/src/piggybank/Main.java
@@ -0,0 +1,33 @@
+// compile commands: javac piggybank/*.java
+//                   jar cvfe piggybank.jar piggybank.Main piggybank/*.class
+//                   java -jar piggybank.jar
+
+package piggybank;
+
+import java.util.*;
+import java.text.DecimalFormat;
+
+public class Main
+{
+    DecimalFormat fp = new DecimalFormat("$###,###.00");
+
+    public static void main(String[] args)
+    {
+
+        // * A Dollar worth $1.00
+        // * A Quarter worth $0.25
+        // * A Dime worth $0.10
+        // * A Nickel worth $0.05
+        // * A Penny worth $0.01 
+
+        // * Create collection
+        // * piggyBank.add(new Quarter())
+        // * piggyBank.add(new Dime())
+        // * piggyBank.add(new Dollar(5))
+        // * piggyBank.add(new Nickel(3))
+        // * piggyBank.add(new Dime(7))
+        // * piggyBank.add(new Dollar())
+        // * piggyBank.add(new Penny(10))
+        // * Print the contents of the Piggy Bank
+    }
+}
