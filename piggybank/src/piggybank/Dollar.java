f3a48e9e437af0486eec7c61268ad6177a0c0132
Diana Grams <deegrams221@gmail.com>
2019-10-04 09:44:45 -0600

    setup Dollar

---
+++ piggybank/src/piggybank/Dollar.java
@@ -0,0 +1,48 @@
+package piggybank;
+
+public class Dollar extends MoneyAbstract
+{
+    // fields
+    private String name;
+    private int count;
+
+    // constructor
+    public Dollar(String name, int count)
+    {
+        super(name, count);
+        moneyValue = 1.00;
+    }
+
+    public Dollar()
+    {
+        moneyValue = 1.00;
+    }
+
+    // from MoneyAbstract
+    @Override
+    public String strValue()
+    {
+        return count + " Dollar";
+    }
+
+    @Override
+    public String strValueInc()
+    {
+        if (this.count > 1)
+        {
+            retrun count + " Dollars";
+        } else 
+        {
+            return strValue();
+        }
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Dollar{" +
+                "name='" + name + '\'' +
+                ", count=" + count + 
+                '}';
+    }
+}
