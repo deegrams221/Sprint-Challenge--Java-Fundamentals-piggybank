ae90f8ecda8eef7a924b572f2ccdc95c5a49ea98
Diana Grams <deegrams221@gmail.com>
2019-10-04 09:45:32 -0600

    setup Quarter

---
+++ piggybank/src/piggybank/Quarter.java
@@ -0,0 +1,48 @@
+package piggybank;
+
+public class Quarter extends MoneyAbstract
+{
+    // fields
+    private String name;
+    private int count;
+
+    // constructor
+    public Quarter(String name, int count)
+    {
+        super(name, count);
+        moneyValue = 0.25;
+    }
+
+    public Quarter()
+    {
+        moneyValue = 0.25;
+    }
+
+    // from MoneyAbstract
+    @Override
+    public String strValue()
+    {
+        return count + " Quarter";
+    }
+
+    @Override
+    public String strValueInc()
+    {
+        if (this.count > 1)
+        {
+            retrun count + " Quarters";
+        } else 
+        {
+            return strValue();
+        }
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Quarter{" +
+                "name='" + name + '\'' +
+                ", count=" + count + 
+                '}';
+    }
+}
